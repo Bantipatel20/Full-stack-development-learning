f572ab82323385f364703e184b46ec388fddf6a7
Bantipatel20 <patelbanti993@gmail.com>
2025-07-09 10:05:02 +0530

    convert kelvin to celcius

--- practical-2/index.js
+++ practical-2/index.js
@@ -10,8 +10,8 @@ async function fetchData(){
     try{
     const respons = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ad355578e1a65b64da61732c5bb798ec`);
     const data = await respons.json();
-    console.log(data);
-    document.getElementById('output').innerText = `${city} whether is ${data.main.temp} c`
+    const temperatureCelsius = (data.main.temp - 273.15).toFixed(2);
+    document.getElementById('output').innerText = `${city} whether is ${temperatureCelsius}°C`
     }catch(e){
         document.getElementById('output').innerText = `${city} not found`;
     }
