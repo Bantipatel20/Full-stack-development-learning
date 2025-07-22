aee604b505d5f0ef5d4658a6d297019dce800c56
Bantipatel20 <patelbanti993@gmail.com>
2025-07-22 11:11:10 +0530

    learn ejs

--- express_learn/app.js
+++ express_learn/app.js
@@ -5,8 +5,21 @@ const app = express()
 const port = 3000
 
 app.use(express.static('public'))
-
-
+app.use(express.urlencoded());
+app.set("views",__dirname+"/views");
+app.set("view engine" , "ejs");
+app.get("/homepage" ,(req,res)=>{
+  res.render("home",{"mya":"kya haal chal"})
+})
+app.get("/marksheet",(req,res)=>{
+  res.render("marksheet");
+})
+app.post("/marksheetprocess",(req,res)=>{
+  var a = req.body.text1
+  var b  = req.body.text2
+  var c  = parseInt(a) + parseInt(b);
+  res.render("app",{"mya":a,"myb":b,"myc":c});
+})
 app.get('/', (req, res) => {
   res.sendFile(__dirname+"/home.html")
 })
@@ -33,6 +46,25 @@ app.get('/contact/ahmedabad', (req, res) => {
   res.send('Hello cake in ahmedabad!')
 })
 
+app.get('/product/:id',(req,res)=>{
+  var id = req.params.id;
+  res.send("Parameter id is = "+ id);
+})
+
+app.get("/sum",(req,res)=>{
+  res.sendFile(__dirname+"/sumdemo.html");
+})
+
+app.get('/sumprocess',(req,res)=>{
+    var a = req.query.text1;
+    var b  = req.query.text2;
+    var c = parseInt(a) + parseInt(b);
+    res.send (`A value is ${a} <br/> B value is ${b} <br/> sum value is ${c}`);
+})
+app.get("/search",(req,res)=>{
+  var qq = req.query.q
+  res.send("Qaury string is the = "+ qq);
+})
 app.listen(port, () => {
   console.log(`Example app listening on port http://127.0.0.1:${port}`)
 })
